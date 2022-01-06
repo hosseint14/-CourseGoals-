dc3a5dce351b2a96457b89781ea9def71a81296d
hosseint14 <htavasol.ht@gmail.com>
2022-01-06 10:10:10 +0330

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import CourseGoalList from "./component/CourseGoals/CourseGoalList/CourseGoalList";
+import CourseInput from "./component/CourseGoals/CourseInput/CourseInput";
+import "./App.css";
 
 function App() {
+  const [courseGoal, setCourseGoals] = useState([
+    { text: "Do all exercises!", id: "e1" },
+    { text: "Finish the course!", id: "e2" },
+  ]);
+
+  const addGoalHandler = (enteredText) => {
+    setCourseGoals((prevGoal) => {
+      const updateGoals = [...prevGoal];
+      updateGoals.unshift({ text: enteredText, id: Math.random().toString() });
+      return updateGoals;
+    });
+  };
+
+  const deleteItemHandler = (goalId) => {
+    setCourseGoals((prevGoal) => {
+      const updateGoals = prevGoal.filter((goal) => goal.id !== goalId);
+      return updateGoals;
+    });
+  };
+  let content = (
+    <p style={{ textAlign: "center" }}>No goals found. Maybe add one?</p>
+  );
+
+  if (courseGoal.length > 0) {
+    content = (
+      <CourseGoalList items={courseGoal} onDelete={deleteItemHandler} />
+    );
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <section id="goal-form">
+        <CourseInput onAdd={addGoalHandler} />
+      </section>
+      <section id="goals">{content}</section>
     </div>
   );
+
+  
 }
 
 export default App;
